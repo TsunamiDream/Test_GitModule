af6144542f49b1bd720ba77488ce3513141418da
Fest <fest.in@gmail.com>
2023-02-20 17:40:52 +0200

    1st

---
+++ com/testLib.cpp
@@ -0,0 +1,13 @@
+/*
+ * testLib
+ *
+ * This file is part of PjS Project.
+ * All rights reserved , distributed under GPLv3+ ,
+ * if not explicitly stated otherwise (see www.gnu.org for details).
+ */
+#include "testLib.h"
+
+namespace pjs {
+
+
+}
---
+++ com/testLib.h
@@ -0,0 +1,25 @@
+/*
+ * testLib
+ *
+ * This file is part of PjS Project.
+ * All rights reserved , distributed under GPLv3+ ,
+ * if not explicitly stated otherwise (see www.gnu.org for details).
+ */
+
+
+#ifndef TEST_GITMODULE_COM_TESTLIB_H
+#define TEST_GITMODULE_COM_TESTLIB_H
+
+
+namespace pjs {
+
+
+/*
+ *
+ */
+class testLib {
+
+};
+}
+
+#endif //TEST_GITMODULE_COM_TESTLIB_H
